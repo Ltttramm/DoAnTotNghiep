44429f243e6855402ac40637f0acb4fbc3d19f1b
Ltttramm <lethithuytram140803@gmail.com>
2025-02-22 15:08:20 +0700

    Khởi tạo project và commit lần đầu

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebsiteQuanLyDinhDuongCaNhan.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            ViewData["HideHeader"] = true;
+            return View();
+        }
+    }
+}
